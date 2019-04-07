042a5cf5527a7f462aed69f757355efcb196f271
wind0406 <255932@qq.com>
2019-04-07 20:00:25 +0800

    update product

--- springcloud-eureka-server/src/main/java/springcloud/eureka/server/SpringcloudEurekaServerApplication.java
+++ springcloud-eureka-server/src/main/java/springcloud/eureka/server/SpringcloudEurekaServerApplication.java
@@ -7,7 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringcloudEurekaServerApplication {
-
+  /**
+   * 服务注册
+   * @param args
+   */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
 	}
